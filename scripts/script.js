2142f5da13c40093726611b1904afc8c649c5aa8
Kaine van Ommeren <kainevommeren@gmail.com>
2022-01-19 20:51:13 +0100

    Small fix JavaScript

--- scripts/script.js
+++ scripts/script.js
@@ -15,33 +15,34 @@ function getAge(dateString) {
     }
     return age;
 }
-document.getElementsById("age").innerHTML = getAge("2000/09/30");
+document.getElementById("age").innerHTML = getAge("2000/09/30");
 
 function toggleMobileMenu(menu)
 {
     menu.classList.toggle('open');
 }
 
-var average = [
-        ["Plannen & Ontwerpen",7],
-        ["Realiseren",7.6],
-        ["Testen & Verbeteren",8.8],
-        ["Computervaardigheden",7.7],
-        ["Burgerschap",8.5],
-        ["Nederlands",6.5],
-        ["Engels",8],
-        ["Rekenen",3.9]
-];
-console.log(average);
-
 function StartedCourse(date) 
 {
-    var monthStarted = new Date();
-    var currentMonth = new Date(date);
-    var d = monthStarted.getMonth() - currentMonth.getMonth();
-    if (d < 0 || (d === 0 && monthStarted.getMonth() < currentMonth.getMonth())) {
-        date--
-    }     
-    return date;
+    var today = new Date();
+    var birthDate = new Date(date);
+    var age = today.getFullYear() - birthDate.getFullYear();
+    var m = today.getMonth() - birthDate.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+        age--;
+    }
+    return age;
 }
-document.getElementById("courseMonth").innerHTML = date("2021/08/01");
+document.getElementById("courseMonth").innerHTML = StartedCourse("2020/08/01");
+
+var average = [
+    ["Plannen & Ontwerpen",7],
+    ["Realiseren",7.6],
+    ["Testen & Verbeteren",8.8],
+    ["Computervaardigheden",7.7],
+    ["Burgerschap",8.5],
+    ["Nederlands",6.5],
+    ["Engels",8],
+    ["Rekenen",3.9]
+];
+console.log(average);
